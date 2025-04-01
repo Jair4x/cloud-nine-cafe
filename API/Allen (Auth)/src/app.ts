b409079ec413367d1dc665ee682bc2984e26b653
Jair4x <Jair4x@gmail.com>
2025-04-01 01:41:01 -0300

    feat(api): Implemented security middleware

--- API/Allen (Auth)/src/app.ts
+++ API/Allen (Auth)/src/app.ts
@@ -1,18 +1,27 @@
+import "dotenv/config";
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
+import hpp from "hpp";
 import authRoutes from "./routes/auth.routes";
 import permRoutes from "./routes/perms.routes";
 import authConfig from "./routes/auth.config";
+import { rateLimiter } from "./middlewares/security";
 
 const app = express();
 
-app.use(cors());
+app.use(helmet());
+app.use(hpp());
+app.use(cors({
+    origin: process.env.MAIN_API_URL,
+    credentials: true,
+    methods: ["GET", "POST"],
+}));
 app.use(express.json());
-
-app.use(authConfig); // Auth configuration middleware
+app.use(authConfig);
 
 //? Routes
-app.use("/v1/auth", authRoutes);
-app.use("/v1/permissions", permRoutes);
+app.use("/v1/auth", rateLimiter(15, "10m"), authRoutes);
+app.use("/v1/permissions", rateLimiter(30, "10m"), permRoutes);
 
 export default app;
